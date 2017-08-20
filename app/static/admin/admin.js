68b0a33ea6d4d915bfff9fea0928dab183379e6f
Darren Hoyland <darren.hoyland@canonical.com>
2017-08-20 11:30:56 +0100

    Removed the flash on reload. Now I've just got the summary to do and then I can get to work on the angularfied survey

--- app/static/admin/admin.js
+++ app/static/admin/admin.js
@@ -8,7 +8,19 @@ hjapp.config(['$routeProvider', function($routeProvider) {
 }]);
 
 hjapp.controller('controller', [
-    '$scope', 'UserService', function($scope, UserService) {
-        $scope.users = UserService.query();
+    '$scope', 'UserService', '$interval', '$q',
+    function($scope, UserService, $interval, $q) {
+        $scope.reload = function () {
+            var user_response = UserService.query();
+            if (angular.isUndefined($scope.users)){
+                $scope.users = user_response;
+            } else{
+                $q.all([user_response.$promise]).then(function(user_data) {
+                    $scope.users = user_data[0]
+                })
+            };
+        };
+        $scope.reload();
+        $interval($scope.reload, 1000);
     }
 ]);
